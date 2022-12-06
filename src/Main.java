91631b1c927c925ba6af5e78163806b89582aa0d
lesdead <lesdead@gmail.com>
2022-12-06 19:51:32 +0200

    all files added1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ballNow = 2_000_000_000;
+        int addManey = 500_000_000;
+        int total = ballNow + addManey;
+        System.out.println(total + " руб.");
+    }
+}
